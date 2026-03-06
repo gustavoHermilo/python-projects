800ad055debcf6e7fc2b5a25d0ef4e1bb46eec46
Gustavo Hermilo <0gustavogarciagarcia@gmail.com>
2026-03-06 09:36:32 -0600

    Implement safe integer reading function

---
+++ python-reading-ints-safely/python-reading-ints-safely.py
@@ -0,0 +1,12 @@
+def read_int(prompt,min,max):
+    while True:
+        try:
+            value=int(input(prompt))
+            if value<min or value>max:
+                print("Error: the value is not within permitted range("+ str(min)+".."+str(max)+ ")")
+            else:
+                return value
+        except ValueError:
+            print("Error: wrong input")
+v= read_int("Enter a number from -10 to 10: ",-10,10)
+print("The number is: ",v)
